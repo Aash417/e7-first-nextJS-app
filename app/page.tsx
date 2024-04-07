e62872330450f2303e67a6e868113bde1c74dda4
aashish_k <aashishkathat00@gmail.com>
2024-04-08 00:53:46 +0530

    instantiate singleton prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '@/db';
 
 export default async function Home() {
 	const user = await prisma.user.findFirst({});
